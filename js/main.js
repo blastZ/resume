2301e639af111d6ccf31fcba80db1917a53e0e06
blastz <touchzh1996@gmail.com>
2017-03-26 23:28:23 +0800

    Git init

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("#pagepiling").pagepiling({
+    verticalCentered: false,
+    navigation: {
+            'textColor': '#fffff',
+            'bulletsColor': '#000000',
+            'position': 'left',
+            'tooltips': ['Home', 'Professions', 'Projects', 'Education', 'Contact Me']
+    },
+    loopBottom: true,
+    scrollingSpeed: 400,
+    afterRender: function(anchorLink, index){
+        $("#arrow").on("click",function(){
+          $.fn.pagepiling.moveSectionDown();
+        });
+    },
+    onLeave: function(index, nextIndex, direction){
+        var $pages = $('.section');
+        for (var i = 0; i < $pages.length; ++i) {
+            if (i + 1 !== nextIndex) {
+                $($pages[i]).addClass('leaving');
+            } else {
+                $($pages[i]).removeClass('leaving');
+            }
+        }
+
+    },
+    afterLoad: function(anchorLink, index){
+    }
+  });
+});
